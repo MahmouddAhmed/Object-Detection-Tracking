004fca45141a792eacaf83431e54410560e25a46
MahmouddAhmed <mahmodahmed98.ma@gmail.com>
2023-01-22 12:22:05 +0100

    added market_1501 dataloader and utilities added reid network model

---
+++ src/utils/market1501_utils.py
@@ -0,0 +1,172 @@
+from __future__ import division, absolute_import
+from collections import defaultdict
+
+import numpy as np
+import datetime
+import torch
+
+
+__all__ = ['AverageMeter', 'MetricMeter']
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value.
+
+    Examples::
+        >>> # Initialize a meter to record loss
+        >>> losses = AverageMeter()
+        >>> # Update meter after every minibatch update
+        >>> losses.update(loss_value, batch_size)
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+class MetricMeter(object):
+    """A collection of metrics.
+
+    Source: https://github.com/KaiyangZhou/Dassl.pytorch
+
+    Examples::
+        >>> # 1. Create an instance of MetricMeter
+        >>> metric = MetricMeter()
+        >>> # 2. Update using a dictionary as input
+        >>> input_dict = {'loss_1': value_1, 'loss_2': value_2}
+        >>> metric.update(input_dict)
+        >>> # 3. Convert to string and print
+        >>> print(str(metric))
+    """
+
+    def __init__(self, delimiter='\t'):
+        self.meters = defaultdict(AverageMeter)
+        self.delimiter = delimiter
+
+    def update(self, input_dict):
+        if input_dict is None:
+            return
+
+        if not isinstance(input_dict, dict):
+            raise TypeError(
+                'Input to MetricMeter.update() must be a dictionary'
+            )
+
+        for k, v in input_dict.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            self.meters[k].update(v)
+
+    def __str__(self):
+        output_str = []
+        for name, meter in self.meters.items():
+            output_str.append(
+                '{} {:.4f} ({:.4f})'.format(name, meter.val, meter.avg)
+            )
+        return self.delimiter.join(output_str)
+
+def extract_features(model, data_loader):
+    f_, pids_, camids_ = [], [], []
+    for data in data_loader:
+        imgs, pids, camids = data['img'], data['pid'], data['camid']
+        imgs = imgs.cuda()
+        features = model(imgs)
+        features = features.cpu().clone()
+        f_.append(features)
+        pids_.extend(pids)
+        camids_.extend(camids)
+    f_ = torch.cat(f_, 0)
+    pids_ = np.asarray(pids_)
+    camids_ = np.asarray(camids_)
+    return f_, pids_, camids_
+
+def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
+    """Evaluation with market1501 metric
+    Key: for each query identity, its gallery images from the same camera view are discarded.
+    """
+    num_q, num_g = distmat.shape
+
+    if num_g < max_rank:
+        max_rank = num_g
+        print(
+            'Note: number of gallery samples is quite small, got {}'.
+            format(num_g)
+        )
+
+    indices = np.argsort(distmat, axis=1)
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+    # compute cmc curve for each query
+    all_cmc = []
+    all_AP = []
+    num_valid_q = 0. # number of valid query
+
+    for q_idx in range(num_q):
+        # get query pid and camid
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+
+        # remove gallery samples that have the same pid and camid with query
+        order = indices[q_idx]
+        remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+        keep = np.invert(remove)
+
+        # compute cmc curve
+        raw_cmc = matches[q_idx][
+            keep] # binary vector, positions with value 1 are correct matches
+        if not np.any(raw_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
+
+        cmc = raw_cmc.cumsum()
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        num_rel = raw_cmc.sum()
+        tmp_cmc = raw_cmc.cumsum()
+        tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
+        tmp_cmc = np.asarray(tmp_cmc) * raw_cmc
+        AP = tmp_cmc.sum() / num_rel
+        all_AP.append(AP)
+
+    assert num_valid_q > 0, 'Error: all query identities do not appear in gallery'
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mAP = np.mean(all_AP)
+
+    return all_cmc, mAP
+
+def print_statistics(batch_idx, num_batches, epoch, max_epoch, batch_time, losses):
+    batches_left = num_batches - (batch_idx + 1)
+    future_batches_left = (max_epoch - (epoch + 1)) * num_batches
+    eta_seconds = batch_time.avg * (batches_left + future_batches_left)
+    eta_str = str(datetime.timedelta(seconds=int(eta_seconds)))
+    print(
+        'epoch: [{0}/{1}][{2}/{3}]\t'
+        'time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+        'eta {eta}\t'
+        '{losses}\t'.format(
+            epoch + 1,
+            max_epoch,
+            batch_idx + 1,
+            num_batches,
+            batch_time=batch_time,
+            eta=eta_str,
+            losses=losses
+        )
+    )
